b53ff04fc54312a8d626f90f74242a458bfc3fc9
idan226688 <idan226688@gmail.com>
2025-04-29 22:42:32 +0300

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/idan.dev',
+  base: '/idan.dev/',
   server: {
     host: "::",
     port: 8080,
